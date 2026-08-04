4549e00e227dd05969cfad15fc6ee973340b9f73
akshat <akshatvijan122205@gmail.com>
2026-08-04 12:23:20 +0530

    uc-5

--- electricCar.py
+++ electricCar.py
@@ -1,7 +1,7 @@
 from vehicle import Vehicle
-class ElectricCar(Vehicle):
+class electricCar(Vehicle):
     def __init__(self,vehicle_id,model,battery_percentage,seating_capacity):
         super().__init__(vehicle_id,model,battery_percentage)
         self.seating_capacity=seating_capacity
     def calculate_trip_cost(self,distance):
-        pass
+        return 5+(0.50*distance)
--- main.py
+++ main.py
@@ -2,16 +2,14 @@ from vehicle import Vehicle
 from electricCar import electricCar
 from electricScooter import electricScooter
 if __name__=="__main__":
-   car1=electricCar("201","car-v200",80,4)
-   scooter1=electricScooter("33","s-c123",20,100)
-   print(car1.get_battery_percentage())
-   car1.set_battery_percentage(20)
-   print(car1.get_battery_percentage())
-   print("status ",scooter1.get_maintenance_status()) 
-   scooter1.set_maintenance_status("Not available")
-   print("status ",scooter1.get_maintenance_status())
-   print(car1.seating_capacity)
-   print(scooter1.max_speed_limit)
+   car1=electricCar("c21","car-200",80,4)
+scooter1=electricScooter("s12","s-302",20,80)
+vehicle=[car1,scooter1]
+for v in vehicle:
+    if isinstance(v,electricCar):
+        print(f"{v.model} fare={v.calculate_trip_cost(20)}")
+    else:
+        print(f"{v.model} fare={v.calculate_trip_cost(10)}")
 
    
 
