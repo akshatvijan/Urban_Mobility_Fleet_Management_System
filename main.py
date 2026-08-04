f642cf671ad874b828411db641b53c2ba907cce0
akshat <akshatvijan122205@gmail.com>
2026-08-04 12:03:07 +0530

    UC2: Add encapsulation with getters and setters

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from vehicle import Vehicle
 if __name__=="__main__":
     v1=Vehicle("v-1","car-1",80)
     print(v1.model)
+    v1.set_battery_percentage(80)
+    v1.set_maintenance_status("Available")
+    v1.set_rental_price(1000)
+    print("battery is ",v1.get_battery_percentage())
+    print("maintenance status is ",v1.get_maintenance_status())
+    print("rental price is ",v1.get_rental_price())
--- vehicle.py
+++ vehicle.py
@@ -2,4 +2,32 @@ class Vehicle:
     def __init__(self,vehicle_id,model,battery_percentage):
         self.vehicle_id=vehicle_id
         self.model=model
-        self.battery_percentage=battery_percentage
+        self.__battery_percentage=battery_percentage
+        self.__maintenance_status="Available"
+        self.__rental_price=0
+        
+#getter for battery percentage
+    def get_battery_percentage(self):
+        return self.__battery_percentage
+
+#setter for battery percentage
+    def set_battery_percentage(self,battery):
+        if not (0<=battery<=100):
+            raise ValueError("Invalid battery percentage!")
+        self.__battery_percentage=battery
+
+#getter for maintenance status
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+#setter for maintenance status
+    def set_maintenance_status(self,status):
+        self.__maintenance_status=status
+
+#getter for rental price
+    def get_rental_price(self):
+        return self.__rental_price
+
+#setter for rental price         
+    def set_rental_price(self,rent):
+        self.__rental_price=rent
