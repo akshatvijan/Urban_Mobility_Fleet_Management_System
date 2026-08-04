dfebf285bd2f8e9e92f0171fe539ce3cf7e46f40
akshat <akshatvijan122205@gmail.com>
2026-08-04 12:16:11 +0530

    uc-3 task

---
+++ electricCar.py
@@ -0,0 +1,5 @@
+from vehicle import Vehicle
+class electricCar(Vehicle):
+    def __init__(self,vehicle_id,model,battery_percentage,seating_capacity):
+        super().__init__(vehicle_id,model,battery_percentage)
+        self.seating_capacity=seating_capacity
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from vehicle import Vehicle
+from electricCar import electricCar
+from electricScooter import electricScooter
 if __name__=="__main__":
-    v1=Vehicle("v-1","car-1",80)
-    print(v1.model)
-    v1.set_battery_percentage(80)
-    v1.set_maintenance_status("Available")
-    v1.set_rental_price(1000)
-    print("battery is ",v1.get_battery_percentage())
-    print("maintenance status is ",v1.get_maintenance_status())
-    print("rental price is ",v1.get_rental_price())
+   car1=electricCar("201","car-v200",80,4)
+   scooter1=electricScooter("33","s-c123",20,100)
+   print(car1.get_battery_percentage())
+   car1.set_battery_percentage(20)
+   print(car1.get_battery_percentage())
+   print("status ",scooter1.get_maintenance_status()) 
+   scooter1.set_maintenance_status("Not available")
+   print("status ",scooter1.get_maintenance_status())
+   print(car1.seating_capacity)
+   print(scooter1.max_speed_limit)
+
+   
+
+    
