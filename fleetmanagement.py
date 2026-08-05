321de556d40fb3af1e157e1515fbb43e0b0340e5
akshat <akshatvijan122205@gmail.com>
2026-08-05 15:31:35 +0530

    Uc-7 updated

--- fleetmanagement.py
+++ fleetmanagement.py
@@ -1,15 +1,39 @@
 from hub import Hub
 class Fleet:
     def __init__(self):
-        self.hubs={}
-    def add_hub(self,hub):
-        if hub not in self.hubs:
-            self.hubs[hub]=[]
-    def add_vehicles(self,hub,vehicle):
-        if hub in self.hubs:
-            if vehicle not in self.hubs[hub]:
-                self.hubs[hub].append(vehicle)
-            else:
-                print("Vehicle already exist")
-        else:
-            print("NO hub found")
+        self.hubs=[]
+    def add_hub(self,hub_name):
+        for hub in self.hubs:
+            if hub.hub_name==hub_name:
+                print("Hub already exist")
+                return
+        new_hub=Hub(hub_name)
+        self.hubs.append(new_hub)
+
+    def add_vehicle(self,hub_name,vehicle):
+        for hub in self.hubs:
+            if hub.hub_name==hub_name:
+               existing=[x for x in hub.vehicles if x==vehicle]
+               if existing:
+                print("vehicle already exist")
+                return
+               else:
+                hub.vehicles.append(vehicle)
+                return
+        
+             
+                
+        print("Hub not found")
+
+    def show_hubs(self):
+        for hub in self.hubs:
+            print(hub.hub_name)
+    def show_vehicles(self,hub_name):
+        for hub in self.hubs:
+            if hub.hub_name==hub_name:
+                for v in hub.vehicles:
+                    print(v)
+                return
+        print("hub not found")
+
+      
--- main.py
+++ main.py
@@ -4,29 +4,21 @@ from electricScooter import electricScooter
 from fleetmanagement import Fleet
 if __name__=="__main__":
     car1=electricCar(201,"car202",80,4)
+    car2=electricCar(201,"car202",80,4)
     scooter1=electricScooter(10,"cs202",80,100)
     scooter2=electricScooter(10,"cs203",81,100)
     scooter3=electricScooter(11,"cs203",81,100)
     f1=Fleet()
     f1.add_hub("Airport")
-<<<<<<< HEAD
-    f1.add_hub("Bus Stand")
-    f1.add_vehicles("Airport",car1)
-    f1.add_vehicles("Bus Stand",scooter1)
-    f1.add_vehicles("Bus Stand",scooter2)
-    f1.add_vehicles("Bus Stand",scooter3)
-    print(f1.hubs)
-    
-    
-=======
     f1.add_hub("Bus-Stand")
     f1.add_vehicle("Airport",car1)
-    f1.add_vehicle("Bus-Stand",scooter1)
-    f1.add_vehicle("Bus-Stand",scooter2)
+    f1.add_vehicle("Airport",car2)
+    # f1.add_vehicle("Bus-Stand",scooter1)
+    # f1.add_vehicle("Bus-Stand",scooter2)
     f1.show_hubs()
-    f1.show_vehicles("Airport")
+    #f1.show_vehicles("Airport")
 
->>>>>>> uc6
+ 
   
    
 
